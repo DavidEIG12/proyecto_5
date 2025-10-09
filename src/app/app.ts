bff79df7c0a56f553e267924a0e78394c447a187
DavidEIG12 <amm24gr025@student.esgerencia.com>
2025-10-09 10:29:40 +0200

    Commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,22 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Persona } from './persona';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Persona],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('proyecto_5');
+  protected readonly title = signal ('proyecto_5');
+
+   persona: Persona = new Persona ('David', 'García', 21);
+
+   personaArray: Persona [] = [
+    new Persona('Ana', 'López', 22),
+    new Persona('Luis', 'Martínez', 23),
+    new Persona('Marta', 'Sánchez', 24),
+    new Persona('Carlos', 'Gómez', 25)
+   ]
 }
---
+++ src/app/persona.ts
@@ -0,0 +1,9 @@
+export class Persona {
+
+    constructor(public nombre:string, public apellidos:string, public edad:number) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.edad = edad;
+    }
+
+}
